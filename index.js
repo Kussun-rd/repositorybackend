243e65429de079961ab8a9207603da29171086a6
Kussun-rd <jhonislachito657@gmail.com>
2025-05-09 10:43:04 -0500

    Modifica servidor para responder a HTTP

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-require('dotenv').config()
-const http= require('http')
+require('dotenv').config();
+const http = require('http');
 
-function requestController(){
-    console.log('Bienvenidos al curso')
+function requestController(req, res) {
+  console.log('Bienvenidos al curso');
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('¡Bienvenidos al curso desde Render!');
 }
 
-const server=http.createServer(requestController)
+const server = http.createServer(requestController);
 
-const PORT=process.env.PORT
+const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, function(){
-    console.log("Aplicacion corriendo en: " + PORT)
-})
+server.listen(PORT, function() {
+  console.log("Aplicacion corriendo en: " + PORT);
+});
